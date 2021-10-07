24668829cb1af86c5c0ec4f90606da8fdfa00c4e
= <samgusaapp1993@gmail.com>
2021-10-06 21:24:37 -0400

    Initial Commit

---
+++ TestPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestPractice
+//
+//  Created by Sam Greenhill on 10/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
